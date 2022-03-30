c68759c498f7841b6535bb33cf950659d638ade6
tarek <tarek.saber.se@gmail.com>
2022-03-30 10:05:13 +0100

    Add whatsapp_message_templates endpoint

--- lib/telnyx/whatsapp_business_accounts.rb
+++ lib/telnyx/whatsapp_business_accounts.rb
@@ -10,6 +10,11 @@ module Telnyx
                                   operations:       [:list],
                                   instance_methods: { list: "phone_number" }
     
+    nested_resource_class_methods "whatsapp_message_template",
+                                  path:             "whatsapp_message_templates",
+                                  operations:       [:list],
+                                  instance_methods: { list: "whatsapp_message_template" }
+    
     OBJECT_NAME   = "whatsapp_business_account".freeze
     RESOURCE_PATH = "whatsapp_business_accounts".freeze
   end
